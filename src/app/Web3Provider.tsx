dafbd14720d9db549672e0f9cb320dcf232e5b3b
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-11-16 14:18:49 +0100

    rpc

--- src/app/Web3Provider.tsx
+++ src/app/Web3Provider.tsx
@@ -13,8 +13,7 @@ export const config = createConfig(
         chains: [mainnet],
         transports: {
             [mainnet.id]: http(
-                "https://virtual.mainnet.eu.rpc.tenderly.co/f76cfa30-c063-497a-8eb1-15dbc0ea3dd0"
-                //getRpcUrl(mainnet.id),
+                getRpcUrl(mainnet.id),
             ),
         },
 
